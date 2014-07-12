c9ce8bf6735c5ec59a8aef8a0bd584392cb06bcd
m1ga <miga_miga@gmx.de>
2014-07-12 18:50:17 +0200

    updated pdf.js version, only one page is loaded now, menu to go to next and previous page

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -7,5 +7,15 @@ var pdf = Ti.Filesystem.getFile(Ti.Filesystem.resourcesDirectory, "pdf.pdf");
 // change the html to open the custom pdf file
 file = file.replace(/pdffile/gi, pdf.nativePath);
 
+function onClickPrev(e) {
+    Ti.App.fireEvent('app:prevPage', {});
+}
+
+function onClickNext(e) {
+    Ti.App.fireEvent('app:nextPage', {});
+}
+
+$.btn_next.addEventListener("click", onClickNext);
+$.btn_prev.addEventListener("click", onClickPrev);
 $.webview.html = file;
 $.index.open();
